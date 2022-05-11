3b72a099f4d38acc86b9241a9e82ad2798168d3f
dtera <huster1005@gmail.com>
2022-05-11 15:12:46 +0800

    [feat]: refactor project

--- proto/bin/init.sh
+++ proto/bin/init.sh
@@ -8,3 +8,10 @@ echo "Current Directory: $CD"
 echo "Work Directory: $WD"
 
 protoc --cpp_out="$WD"/gen --proto_path "$WD" mmdcotservicehttpbroker.proto
+#cd "$WD"/gen/
+#rm -f mmdcotservicehttpbroker_pb*
+#mv mmdcotservicehttpbroker.pb.cc mmdcotservicehttpbroker_pb.cc
+#mv mmdcotservicehttpbroker.pb.h mmdcotservicehttpbroker_pb.h
+##brew install gnu-sed
+#gsed -i 's/mmdcotservicehttpbroker\.pb\.h/mmdcotservicehttpbroker_pb\.h/g' mmdcotservicehttpbroker_pb.cc
+#git add mmdcotservicehttpbroker_pb.cc mmdcotservicehttpbroker_pb.h
