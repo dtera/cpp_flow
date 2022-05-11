e60c27009fc1cd90ec31b61e56eda8709893ed78
dtera <huster1005@gmail.com>
2022-05-11 12:10:11 +0800

    [feat]: refactor project

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 #include "tests/Tests.h"
 
-int main() {
+int main(int argc, const char *argv[]) {
     clock_t start_time = clock();
+    ParamsTool::fromArgs(argc, argv);
     test();
     clock_t end_time = clock();
     cout << "\nThe run time is: " << (double) (end_time - start_time) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
