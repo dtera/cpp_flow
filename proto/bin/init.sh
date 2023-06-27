21ec6be9d147d2f24e8f00469160ff6e8e124b48
dtrea <45172737+dtera@users.noreply.github.com>
2023-06-27 12:10:45 +0800

    Update init.sh

--- proto/bin/init.sh
+++ proto/bin/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CD=$(cd "$(dirname "$0")" || exit; pwd)
-WD=$(cd "$(dirname "$CD")" || exit; pwd)
+CD=$(cd "$(dirname "$0")" || exit && pwd)
+WD=$(cd "$(dirname "$CD")" || exit && pwd)
 echo "Current Directory: $CD"
 echo "Work Directory: $WD"
 
